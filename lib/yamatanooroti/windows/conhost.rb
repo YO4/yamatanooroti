23212e688f4abf2f1d2c9b9ce7ac1f70d7769714
YO4 <ysno@ac.auone-net.jp>
2024-11-04 17:20:30 +0900

    Ctrl+C resistant for conhost

--- lib/yamatanooroti/windows/conhost.rb
+++ lib/yamatanooroti/windows/conhost.rb
@@ -17,15 +17,28 @@ class Yamatanooroti::ConhostTerm
     @wrote_and_not_yet_waited = false
 
     countup_testcase_title(name)
-    pipename = get_pipename(name)
-    @pipe_handle = DL.create_named_pipe(pipename)
+    pipename = get_pipename(name, "open")
+    pipe_handle = DL.create_named_pipe(pipename)
     DL.create_console(keeper_commandline(pipename), show_console_param())
 
     # wait for console startup complete
-    with_timeout("Console process startup timed out.") do
+    with_timeout("Console opening process startup timed out.") do
+      @console_process_id = DL.get_named_pipe_client_processid(pipe_handle, maybe_fail: true)
+    end
+
+    pipename = get_pipename(name, "main")
+    @pipe_handle = DL.create_named_pipe(pipename)
+    attach_terminal(open: false) do
+      spawn(keeper_commandline(pipename))
+    end
+
+    # wait for console startup complete
+    with_timeout("Console keeping process startup timed out.") do
       @console_process_id = DL.get_named_pipe_client_processid(@pipe_handle, maybe_fail: true)
     end
 
+    DL.close_handle(pipe_handle)
+    sleep 0.1
     setup_cp(codepage) if codepage
 
     attach_terminal do |conin, conout|
